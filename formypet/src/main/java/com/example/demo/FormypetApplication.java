239e565bae2d05b5e86a8aadad3249af63e97401
why31 <why31@DESKTOP-614R248>
2024-02-29 17:26:07 +0900

    세팅

---
+++ formypet/src/main/java/com/example/demo/FormypetApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormypetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormypetApplication.class, args);
+	}
+
+}
