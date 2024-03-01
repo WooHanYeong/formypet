cda3f8814e73e2bf8e56283f0990f1fac76fb26d
why31 <why31@DESKTOP-614R248>
2024-03-01 14:38:00 +0900

    작업중

---
+++ formypet/src/main/java/com/formypet/jpa/Controller.java
@@ -0,0 +1,15 @@
+package com.formypet.jpa;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+@RequestMapping("/")
+public class Controller {
+
+	@GetMapping("/index")
+	public String index() {
+		String forwardPath = "index";
+		return forwardPath;
+	}
+}
--- formypet/src/main/java/com/formypet/jpa/user/controller/UserController.java
+++ formypet/src/main/java/com/formypet/jpa/user/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.formypet.jpa.user.controller;
 
-public class UserController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@org.springframework.stereotype.Controller
+@RequestMapping("/")
+public class UserController {
+	
+	@GetMapping("/user_login_form")
+	public String user_login_form() {
+		String forwardPath = "user_login_form";
+		return forwardPath;
+	}
 }
