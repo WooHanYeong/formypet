513e370446401a41eab11f975ce8e27a32a9cf6f
why31 <why31@DESKTOP-614R248>
2024-03-02 01:24:35 +0900

    카드 디자인

--- formypet/src/main/java/com/formypet/jpa/cart/controller/CartController.java
+++ formypet/src/main/java/com/formypet/jpa/cart/controller/CartController.java
@@ -1,5 +1,15 @@
 package com.formypet.jpa.cart.controller;
 
-public class CartController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@org.springframework.stereotype.Controller
+@RequestMapping("/")
+
+public class CartController {
+	@GetMapping("/cart_detail")
+	public String cart_detail() {
+		String forwardPath = "cart_detail";
+		return forwardPath;
+	}
 }
--- formypet/src/main/java/com/formypet/jpa/product/controller/ProductController.java
+++ formypet/src/main/java/com/formypet/jpa/product/controller/ProductController.java
@@ -17,4 +17,14 @@ public class ProductController {
 		String forwardPath = "product_list_cat";
 		return forwardPath;
 	}
+	@GetMapping("/product_detail_dog")
+	public String product_detail_dog() {
+		String forwardPath = "product_detail_dog";
+		return forwardPath;
+	}
+	@GetMapping("/product_detail_cat")
+	public String product_detail_cat() {
+		String forwardPath = "product_detail_cat";
+		return forwardPath;
+	}
 }
