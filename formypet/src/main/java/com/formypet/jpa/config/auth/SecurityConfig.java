59951b865dac28786dd59dbf1dbd650dba4d111b
why31 <why31@DESKTOP-614R248>
2024-03-04 04:43:21 +0900

    잔다

--- formypet/src/main/java/com/formypet/jpa/config/auth/SecurityConfig.java
+++ formypet/src/main/java/com/formypet/jpa/config/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import com.formypet.jpa.user.dto.Role;
 import com.formypet.jpa.user.service.CustomOAuth2UserService;
@@ -16,37 +17,29 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final LogoutHandler logoutHandler;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(
-                        (csrfConfig) -> csrfConfig.disable()
-                )
-                .headers(
-                        (headerConfig) -> headerConfig.frameOptions(
-                                frameOptionsConfig -> frameOptionsConfig.disable()
-                        )
-                )
-                .authorizeHttpRequests((authorizeRequest) -> authorizeRequest
-                        .requestMatchers("/posts/new", "/comments/save").hasRole(Role.USER.name())
-                        .requestMatchers("/", "/css/**", "images/**", "/js/**", "/login/*", "/logout/*", "/posts/**", "/comments/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .logout( // 로그아웃 성공 시 / 주소로 이동
-                        (logoutConfig) -> logoutConfig.logoutSuccessUrl("/")
-                )
-                // OAuth2 로그인 기능에 대한 여러 설정
-        
-                .oauth2Login(
-                        (oauth) ->
-                            oauth.userInfoEndpoint(
-                                    (endpoint) -> endpoint.userService(customOAuth2UserService)
-                            )
-                );
-        
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf((csrfConfig) -> csrfConfig.disable())
+				.headers(
+						(headerConfig) -> headerConfig.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()))
+				.authorizeHttpRequests((authorizeRequest) -> authorizeRequest
+						.requestMatchers("/posts/new", "/comments/save").hasRole(Role.USER.name())
+						.requestMatchers("/", "/css/**", "images/**", "/js/**", "/login/*", "/logout/*", "/posts/**",
+								"/comments/**")
+						.permitAll().anyRequest().authenticated())
+				// 로그아웃 설정
+				.logout((logoutConfig) -> logoutConfig.logoutUrl("/logout") // 로그아웃 URL
+													   .invalidateHttpSession(true)
+													   .logoutSuccessUrl("/user_login_form") // 로그아웃 성공 시 이동할 URL
+													   
+				)
+				// OAuth2 로그인 기능에 대한 여러 설정
+				.oauth2Login(
+						(oauth) -> oauth.userInfoEndpoint((endpoint) -> endpoint.userService(customOAuth2UserService)));
 
-        return http.build();
-    }
+		return http.build();
+	}
 }
