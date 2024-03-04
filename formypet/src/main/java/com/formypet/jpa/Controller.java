e251dc2651c372ae44cda66b960c238a5b8a5f34
why31 <why31@DESKTOP-614R248>
2024-03-04 16:41:50 +0900

    1

--- formypet/src/main/java/com/formypet/jpa/Controller.java
+++ formypet/src/main/java/com/formypet/jpa/Controller.java
@@ -3,12 +3,26 @@ package com.formypet.jpa;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.formypet.jpa.user.dto.SessionUser;
+
+import ch.qos.logback.core.model.Model;
+import jakarta.servlet.http.HttpSession;
+
 @org.springframework.stereotype.Controller
 @RequestMapping("/")
 public class Controller {
+	
+	private HttpSession httpSession;
 
 	@GetMapping("/index")
-	public String index() {
+	public String index(org.springframework.ui.Model model) {
+		if(httpSession !=null) {
+			SessionUser user = (SessionUser) httpSession.getAttribute("user");
+			
+			if(user!=null) {
+				model.addAttribute("userName",user.getName());
+			}
+		}
 		String forwardPath = "index";
 		return forwardPath;
 	}
