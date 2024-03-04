f5c9e22fb3137e4a40ca6ac73e491f088f119900
why31 <why31@DESKTOP-614R248>
2024-03-04 16:59:04 +0900

    ㅋㅋ

--- formypet/src/main/java/com/formypet/jpa/user/handler/CustomLogoutHandler.java
+++ formypet/src/main/java/com/formypet/jpa/user/handler/CustomLogoutHandler.java
@@ -1,4 +1,4 @@
-package com.formypet.jpa.handler;
+package com.formypet.jpa.user.handler;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
