b79da0149cec15af82ef9a9a6f38c34e7621795e
why31 <why31@DESKTOP-614R248>
2024-03-26 16:45:44 +0900

    ing

--- formypet/src/main/java/com/formypet/jpa/animal/repository/AnimalRepository.java
+++ formypet/src/main/java/com/formypet/jpa/animal/repository/AnimalRepository.java
@@ -8,5 +8,6 @@ import com.formypet.jpa.animal.entity.Animal;
 
 public interface AnimalRepository extends JpaRepository<Animal, Long>{
 
+	//강아지 리스트 최신순
 	List<Animal> findByAnimalType_OrderByCreatedTimeDesc(String animaltype);
 }
--- formypet/src/main/java/com/formypet/jpa/animal/service/AnimalServiceImpl.java
+++ formypet/src/main/java/com/formypet/jpa/animal/service/AnimalServiceImpl.java
@@ -1,5 +1,21 @@
 package com.formypet.jpa.animal.service;
 
+import java.util.List;
+
+import com.formypet.jpa.animal.entity.Animal;
+
 public class AnimalServiceImpl implements AnimalService{
+
+	@Override
+	public Animal insert(Animal animal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Animal> findByAnimalType_OrderByCreatedTimeDesc(String animaltype) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
