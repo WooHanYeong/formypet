647606ee4a78a72f9a606d31bdf21de7e7aa897f
why31 <why31@DESKTOP-614R248>
2024-03-31 23:23:41 +0900

    ㅇ

--- formypet/src/main/resources/sql/user-insert.sql
+++ formypet/src/main/resources/sql/user-insert.sql
@@ -1,6 +1,6 @@
 /*
 INSERT INTO userentity(USER_ID, USER_PASSWORD, USER_NAME, USER_ADDRESS, USER_ADDRESS_DETAIL,USER_BIRTH_DATE) 
-VALUES('admin', 'admin', '관리자', '서울 강남구 가로수길 5 (신사동)','아이티윌',19960410);
+VALUES('admin', 'admin', '관리자', '서울 강남구 가로수길 5 (신사동)','관리자집',19960410);
 
 INSERT INTO userentity(USER_ID, USER_PASSWORD, USER_NAME, USER_ADDRESS, USER_ADDRESS_DETAIL,USER_BIRTH_DATE) 
 VALUES('96why', '0410', '우한영', '경기도 안양시 수리산로11','3동1호',19960410);
